f9db7e3a24dfc8e25a50c43117bc8d905d285ee7
Alexander <frosti-sizov@rambler.ru>
2024-04-22 15:29:27 +0400

    added login and registration

--- src/main/java/com/group/web/service/UserService.java
+++ src/main/java/com/group/web/service/UserService.java
@@ -1,7 +1,12 @@
 package com.group.web.service;
 
 import com.group.web.dto.RegistrationDto;
+import com.group.web.models.UserEntity;
 
 public interface UserService {
     void saveUser(RegistrationDto registrationDto);
+
+    UserEntity findByEmail(String email);
+
+    UserEntity findByUsername(String username);
 }
--- src/main/java/com/group/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/group/web/service/impl/UserServiceImpl.java
@@ -7,9 +7,10 @@ import com.group.web.repository.RoleRepository;
 import com.group.web.repository.UserRepository;
 import com.group.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+@Service
 
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
@@ -31,5 +32,15 @@ public class UserServiceImpl implements UserService {
         user.setRoles(Arrays.asList(role));
         userRepository.save(user);
 
+    }
+
+    @Override
+    public UserEntity findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserEntity findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
